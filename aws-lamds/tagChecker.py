7750d96622c9583378d30f5c690a169ee3ca5825
Jayasimha Reddy Chinna Ramanna Gari <jayasimhareddy.chinnaramannagari@nxp.com>
2020-05-05 13:36:35 +0530

    Added ymal input method

--- aws-lamds/tagChecker.py
+++ aws-lamds/tagChecker.py
@@ -5,18 +5,26 @@ import os
 import boto3
 import yaml
 
-def send_mail(message):
-    sns = boto3.client('sns')
+sns_arnList = []
+regions = []
+requiredTagsList = []
+
+def read_yaml_input():
+    global regions,sns_arnList,requiredTagsList
     with open(r'./sns_arn.yml') as file:
         sns_arnList = yaml.full_load(file)
-    response = sns.publish( TopicArn=sns_arnList[0],Message=message,)
-    print(response)
-def lambda_handler(event, context):
-    # TODO implement
     with open(r'./regions.yml') as file:
         regions = yaml.full_load(file)
     with open(r'./tags.yml') as file:
         requiredTagsList =  yaml.full_load(file)
+
+def send_mail(message):
+    sns = boto3.client('sns')
+    response = sns.publish( TopicArn=sns_arnList[0],Message=message,)
+    print(response)
+def lambda_handler(event, context):
+    # TODO implement
+    read_yaml_input()
     for region in regions["regions"]:
         tagLessInstanceList=[]
         ec2client = boto3.client('ec2',region_name=region)
